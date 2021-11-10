faa54e87666625f7634bbb2d9bb3997286f23ae0
lucas marcolino <lucas.marcolino32@gmail.com>
2021-11-10 20:36:42 -0300

    alterando o menu

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+include 'cabecalho.php';
+?>
+
+<body>
+    <div class="card">
+        <div id="menu-img1">
+            <!--<a href="index.html">
+                <img src="imagens/logo.png">-->
+                <h1>Imagem</h1>
+            </a>
+        </div>
+        <div id="menu-div1">
+            <a href="index.html">Inicio</a>
+        </div>
+        <div id="menu-div2">
+            <a href="#">Sobre nos</a>
+        </div>
+        <div id="menu-div3">
+            <a href="#">Nossos produtos</a>
+        </div>
+        <div id="menu-div4">
+            <a href="#">Contato</a>
+        </div>
+    </div>
+    <!-- <section id="secao1">
+        <div>
+            <h1>Lorem ipsum dolor sit.</h1>
+            <img src="imagens/home3.png" alt="computador" class="home">
+        </div>
+    </section> -->
+</body>
+</html>
