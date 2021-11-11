0f5e1629b471d9c14544ba7852648d139dcc6692
lucas marcolino <lucas.marcolino32@gmail.com>
2021-11-10 22:45:18 -0300

    alterando o menu e incluido div's

---
+++ home.php
@@ -0,0 +1,49 @@
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      <img src="imagens/missao.jpeg" alt="Avatar">
+    </div>
+    <div class="flip-card-back">
+    <br>
+    <br>
+      <h1>MISSAO</h1>
+      <br>
+      <br>
+      <br>
+      <p>Responder de maneira proativa às necessidades dos clientes com o objetivo 
+          de manter sua infraestrutura em TI sempre em segurança e disponível para 
+          seu core business, além de trabalhar com transparência e pontualidade, 
+          superando as expectativas de nossos clientes.</p>
+    </div>
+  </div>
+</div>
+
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      <img src="imagens/missao.jpeg" alt="Avatar">
+    </div>
+    <div class="flip-card-back">
+    <br>
+    <br>
+      <h1>MISSAO</h1>
+      <br>
+      <br>
+      <br>
+      <p>Responder de maneira proativa às necessidades dos clientes com o objetivo 
+          de manter sua infraestrutura em TI sempre em segurança e disponível para 
+          seu core business, além de trabalhar com transparência e pontualidade, 
+          superando as expectativas de nossos clientes.</p>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,33 +1,5 @@
 <?php
-include 'cabecalho.php';
+include_once 'cabecalho.php';
+include_once 'menu.php';
+include_once 'home.php';
 ?>
-
-<body>
-    <div class="card">
-        <div id="menu-img1">
-            <!--<a href="index.html">
-                <img src="imagens/logo.png">-->
-                <h1>Imagem</h1>
-            </a>
-        </div>
-        <div id="menu-div1">
-            <a href="index.html">Inicio</a>
-        </div>
-        <div id="menu-div2">
-            <a href="#">Sobre nos</a>
-        </div>
-        <div id="menu-div3">
-            <a href="#">Nossos produtos</a>
-        </div>
-        <div id="menu-div4">
-            <a href="#">Contato</a>
-        </div>
-    </div>
-    <!-- <section id="secao1">
-        <div>
-            <h1>Lorem ipsum dolor sit.</h1>
-            <img src="imagens/home3.png" alt="computador" class="home">
-        </div>
-    </section> -->
-</body>
-</html>
