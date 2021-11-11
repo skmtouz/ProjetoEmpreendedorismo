264848b4b060d03befa131f545a1c56638722473
GVFerreira <88843638+GVFerreira@users.noreply.github.com>
2021-11-10 23:23:58 -0300

    alteração no logo e inclusão dos outros cards

--- home.php
+++ home.php
@@ -40,7 +40,24 @@
       <br>
       <br>
       <br>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam laborum dignissimos atque molestiae numquam nihil corrupti animi, voluptas ipsum maiores beatae quis consectetur laboriosam assumenda tenetur, eveniet totam? A perspiciatis hic nisi nostrum accusantium officiis praesentium ea quos obcaecati quaerat dolores culpa est illum, aspernatur eligendi voluptate rem vero cum!</p>
+      <p>Responder de maneira proativa às necessidades dos clientes com o objetivo 
+          de manter sua infraestrutura em TI sempre em segurança e disponível para 
+          seu core business, além de trabalhar com transparência e pontualidade, 
+          superando as expectativas de nossos clientes.</p>
+    </div>
+    
+  </div>
+</div>
+
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      <img src="imagens/missao.png" alt="Avatar">
     </div>
     <div class="flip-card-back">
       <br>
@@ -49,7 +66,11 @@
       <br>
       <br>
       <br>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nesciunt modi nemo alias, nam voluptatum totam beatae ipsum sapiente fugit et quas, tenetur blanditiis iste asperiores ullam cum delectus itaque dolor laudantium, reprehenderit voluptate enim! Perferendis provident nihil, architecto sed non officia sunt sapiente necessitatibus nesciunt maxime veniam aut excepturi!</p>
+      <p>Responder de maneira proativa às necessidades dos clientes com o objetivo 
+          de manter sua infraestrutura em TI sempre em segurança e disponível para 
+          seu core business, além de trabalhar com transparência e pontualidade, 
+          superando as expectativas de nossos clientes.</p>
     </div>
+    
   </div>
 </div>
